e850d1b8eee7a33393c4c21a6e7e3a66da282234
Tatsunori Uchino <tats.u@live.jp>
2018-08-12 15:26:24 +0900

    Add option to halve size of array

--- batch_norm_test.py
+++ batch_norm_test.py
@@ -2,6 +2,7 @@
 
 import os
 import ssl
+from argparse import ArgumentParser
 from copy import copy
 from functools import reduce
 from operator import add
@@ -21,7 +22,7 @@ from tensorflow.keras.layers import (
     MaxPooling2D,
 )
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, Optimizer
 from tensorflow.keras.utils import to_categorical
 
 
@@ -64,11 +65,27 @@ class MNISTEvaluator:
         ssl._create_default_https_context = ssl._create_default_https_context
         return data
 
-    def __init__(self, optimizer):
+    @staticmethod
+    def normalize_x(x: np.ndarray) -> np.ndarray:
+        # Normalize to [0, 1] & Reshape to 4 dimensions
+        return (x.astype(np.float32) / 255.).reshape(x.shape + (1,))
+
+    @staticmethod
+    def normalize_y(y: np.ndarray) -> np.ndarray:
+        # Convert labels to one-hot
+        return to_categorical(y, 10)
+
+    @staticmethod
+    def halve_array(array: np.ndarray) -> np.ndarray:
+        return array[: array.shape[0] // 2]
+
+    def __init__(self, optimizer: Optimizer, halve_data: bool) -> None:
         (x_train, y_train), (
             x_test,
             y_test,
         ) = MNISTEvaluator.fetch_mnist_data()
+
+        print(f"Data: {x_train.shape[0]} (Training) / {x_test} (Test)")
 
         # Verify data
         self.per_data_dimension = x_train.shape[1:]
@@ -77,22 +94,24 @@ class MNISTEvaluator:
 
         # Preprocess data for NN
 
-        # Normalize to [0, 1] & Reshape to 4 dimensions
-        self.x_train = (x_train.astype(np.float32) / 255.).reshape(
-            x_train.shape + (1,)
+        self.x_train = MNISTEvaluator.normalize_x(
+            MNISTEvaluator.halve_array(x_train) if halve_data else x_train
         )
-        self.x_test = (x_test.astype(np.float32) / 255.).reshape(
-            (x_test.shape + (1,))
+        self.x_test = MNISTEvaluator.normalize_x(
+            MNISTEvaluator.halve_array(x_test) if halve_data else x_test
         )
 
-        # Convert labels to one-hot
-        self.y_train = to_categorical(y_train, 10)
-        self.y_test = to_categorical(y_test, 10)
+        self.y_train = MNISTEvaluator.normalize_y(
+            MNISTEvaluator.halve_array(y_train) if halve_data else y_train
+        )
+        self.y_test = MNISTEvaluator.normalize_y(
+            MNISTEvaluator.halve_array(y_test) if halve_data else y_test
+        )
 
         self.optimizer = optimizer
 
     @staticmethod
-    def plot_losses(history, title, out=None):
+    def plot_losses(history, title: str, out=None) -> None:
         fig = plt.figure()
         ax1 = fig.add_subplot(211)
         ax2 = fig.add_subplot(212)
@@ -112,9 +131,7 @@ class MNISTEvaluator:
         plt.savefig(out, transparent=True)
         plt.close(fig)
 
-
-    def train_and_evaluate(self, model_label, model):
-
+    def train_and_evaluate(self, model_label, model) -> None:
         print(f"** {model_label} **")
         model.compile(
             loss="categorical_crossentropy",
@@ -123,15 +140,22 @@ class MNISTEvaluator:
         )
         start_time = time()
         history = model.fit(
-            self.x_train, self.y_train, validation_data=(self.x_test, self.y_test), epochs=6
+            self.x_train,
+            self.y_train,
+            validation_data=(self.x_test, self.y_test),
+            epochs=6,
         )
         elapsed_seconds = time() - start_time
-        print("Learning time: {:.03f} sec".format(elapsed_seconds))
+        print(f"Learning time: {elapsed_seconds:.03f} sec")
 
         loss, accuracy = model.evaluate(self.x_test, self.y_test)
-        print("Loss: {} / Accuracy: {}".format(loss, accuracy))
+        print(f"Loss: {loss} / Accuracy: {accuracy}")
 
-        MNISTEvaluator.plot_losses(history.history, model_label + "(Time: {:.03f} s)".format(elapsed_seconds), model_label + ".pdf")
+        MNISTEvaluator.plot_losses(
+            history.history,
+            f"{model_label} (Time: {elapsed_seconds:.03f} s)",
+            f"{model_label}.pdf",
+        )
 
     def make_cnn_model(self, builder: ModelBuilder):
         builder.add(
@@ -162,17 +186,25 @@ class MNISTEvaluator:
         builder.add(Dense(10, activation="softmax"))
         return builder.build()
 
-if os.name != "nt" and "DISPLAY" not in os.environ:
-    plt.switch_backend("Agg")
 
-evaluator = MNISTEvaluator(Adam())
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--halve-data", action="store_true", help="Halve data")
+    args = parser.parse_args()
 
-# Make CNN models
-model_with_bn = evaluator.make_cnn_model(ModelBuilder(Sequential()))
-model_without_bn = evaluator.make_cnn_model(
-    BatchNormalizationLessModelBuilder(Sequential())
-)
+    if os.name != "nt" and "DISPLAY" not in os.environ:
+        plt.switch_backend("Agg")
 
-# Train & evaluate
-evaluator.train_and_evaluate("with Batch Normalization", model_with_bn)
-evaluator.train_and_evaluate("without Batch Normalization", model_without_bn)
+    evaluator = MNISTEvaluator(Adam(), args.halve_data)
+
+    # Make CNN models
+    model_with_bn = evaluator.make_cnn_model(ModelBuilder(Sequential()))
+    model_without_bn = evaluator.make_cnn_model(
+        BatchNormalizationLessModelBuilder(Sequential())
+    )
+
+    # Train & evaluate
+    evaluator.train_and_evaluate("with Batch Normalization", model_with_bn)
+    evaluator.train_and_evaluate(
+        "without Batch Normalization", model_without_bn
+    )
